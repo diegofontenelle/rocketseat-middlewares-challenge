a0bcee1d80b04a4a9bba0af58d4a929f2951ecf3
Diego Fontenelle <diego.lima.fontenelle@gmail.com>
2021-06-05 17:20:53 -0300

    implement middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,54 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(u => u.username === username);
+
+  if (user) {
+    request.user = user;
+    return next();
+  }
+
+  return response.status(404).json({ error: 'Usuário não encontrado' });
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro || user.todos.length < 10) {
+    return next();
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(u => u.username === username);
+
+  const userTodo = user.todos.find(todo => todo.id === id);
+
+  if (user && userTodo && validate(id)) {
+    request.todo = userTodo;
+    request.user = user;
+
+    return next();
+  }
+
+  return response.status(404).json({ error: 'Todo não encontrado' });
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(u => u.id === id);
+
+  if (user) {
+    request.user = user;
+
+    return next();
+  }
 }
 
 app.post('/users', (request, response) => {
